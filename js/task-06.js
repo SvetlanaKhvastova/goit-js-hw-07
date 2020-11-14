c5ba8da656ca4023d2d2a92e81eb738b4b93e158
Svetlana Khvastova <svetakhvastova@gmail.com>
2020-11-14 14:26:07 +0200

    task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,5 +1,14 @@
 const inputRef = document.getElementById('validation-input');
 
-// inputRef.addEventListener('focus', () => {
-//   inputRef.classList.add(valid);
-// });
+inputRef.addEventListener('change', event => {
+  if (event.target.value.length === +inputRef.dataset.length) {
+    inputRef.classList.add('valid');
+    inputRef.classList.remove('invalid');
+  } else if (event.target.value.length === 0) {
+    inputRef.classList.remove('valid');
+    inputRef.classList.remove('invalid');
+  } else {
+    inputRef.classList.remove('valid');
+    inputRef.classList.add('invalid');
+  }
+});
